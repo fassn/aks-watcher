2197d20f74c2f24cb338a6f32d2e6cdfb061702f
Christopher Fargere <christopher.fargere@gmail.com>
2022-03-24 02:17:13 +0100

    front improvements on form and gamecards

--- components/gameCard.tsx
+++ components/gameCard.tsx
@@ -1,27 +1,48 @@
 import Image from "next/image"
+import Link from "next/link"
 import { Game } from "../pages/api/helpers/game"
 
+const refreshPrice = () => {
+    fetch('/api/games/refresh').then()
+}
+
 export const GameCard = (props: { game: Game }) => {
+    const locale = process.env.NEXT_PUBLIC_LOCALE
     return (
         <div className="flex justify-center">
-            <div className="w-64 my-10 border-solid border-2 shadow-md shadow-deep-blue">
-                <div className="flex">
-                    <Image
-                        src={ props.game.cover }
-                        alt={ props.game.name + ' game cover'}
-                        width='256'
-                        height='256'
-                    />
+            <div className="w-64 my-10 outline outline-2 shadow shadow-light-grey">
+                <div className="flex h-64">
+                    <Link href={props.game.url}>
+                        <a>
+                            <Image
+                                src={ props.game.cover }
+                                alt={ props.game.name + ' game cover'}
+                                width='256'
+                                height='256'
+                            />
+                        </a>
+                    </Link>
                 </div>
-                <div className="flex h-40 space-x-3 px-4 py-6 font-josephin bg-light-grey">
-                    <span className="w-20 h-fit rounded-lg bg-deep-blue text-center text-cream font-semibold">{ props.game.platform }</span>
-                    <div>
-                        <pre className="inline-block font-josephin">Best Price: </pre>
-                        <span className="font-semibold">{ props.game.bestPrice } €</span>
+                <div className="flex flex-col h-40 px-4 py-6 font-josephin bg-light-grey">
+                    <div className="flex space-x-3">
+                        <span className="w-16 h-fit rounded-lg bg-deep-blue text-center text-cream font-semibold">{ props.game.platform }</span>
+                        <div>
+                            <pre className="inline-block font-josephin">Best Price: </pre>
+                            <span className="font-semibold text-deep-blue">{ props.game.bestPrice }€</span>
+                        </div>
+                    </div>
+                    <div className="flex h-full justify-center items-end text-sm">
+                        <pre className="inline-block font-josephin">Last updated: </pre>
+                        <span className="font-semibold text-deep-blue">{ new Date(props.game.dateUpdated).toLocaleDateString(locale) }</span>
+                        <button onClick={refreshPrice} className="w-12">
+                            <svg className="inline-block h-4 w-4 text-deep-blue" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -5v5h5" />  <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 5v-5h-5" /></svg>
+                        </button>
                     </div>
                 </div>
-                <div className="flex justify-center items-center h-16 bg-deep-blue text-center uppercase text-xl leading-6">
-                    { props.game.name }
+                <div className="flex justify-center items-center h-16 bg-deep-blue text-cream text-center uppercase text-xl leading-6">
+                    <Link href={props.game.url}>
+                        <a>{ props.game.name }</a>
+                    </Link>
                 </div>
             </div>
         </div>
--- components/gameForm.tsx
+++ components/gameForm.tsx
@@ -25,24 +25,38 @@ export const GameForm = () => {
         router.push('/')
     }
 
+    // Can easily add new patterns if AllKeyShop URLs were to change in the future.
+    const regexPatterns = [
+        'https:\/\/www\.allkeyshop\.com\/blog\/buy-[A-Za-z0-9\-]+compare-prices\/?',
+        'https:\/\/www\.allkeyshop\.com\/blog\/compare-and-buy-cd-key-for-digital-download-[A-Za-z0-9\-]+\/?'
+    ]
+    const pattern = regexPatterns.join('|')
+
     return (
         <>
             <form onSubmit={handleSubmit} method='post'>
-                <div>
-                    Paste the AllKeyShop link. Example:<br />
-                    <i>https://www.allkeyshop.com/blog/buy-doom-eternal-cd-key-compare-prices/</i>
+                <div className="flex flex-col h-96 bg-light-grey font-josephin border-solid border-2 px-4 pt-10 pb-4 shadow">
+                    <div className="w-full mb-3">
+                        Paste an AllKeyShop link:<br />
+                        <i className="text-xs break-words">https://www.allkeyshop.com/blog/buy-doom-eternal-cd-key-compare-prices/</i>
+                    </div>
+                    <div className="">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="aksLink">AllKeyShop Link:</label>
+                        <input
+                            className="shadow appearance-none border border-deep-blue rounded w-full py-2 px-3 text-deep-blue mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                            type='text'
+                            id='aksLink'
+                            name='aksLink'
+                            placeholder='https://www.allkeyshop.com/...'
+                            pattern={pattern}
+                            title='Invalid link. Please check the description above for an example.'
+                            required
+                        />
+                    </div>
+                    <div className="flex h-full">
+                        <button className="flex w-full justify-center self-end bg-deep-blue text-cream font-semibold py-2 px-4 border border-blue-500 hover:border-transparent rounded" type='submit'>Add Game</button>
+                    </div>
                 </div>
-                <label htmlFor="aksLink">AllKeyShop Link</label>
-                <input
-                    type='text'
-                    id='aksLink'
-                    name='aksLink'
-                    placeholder='https://www.allkeyshop...'
-                    pattern="https:\/\/www\.allkeyshop\.com\/blog\/buy-[A-Za-z0-9\-]+compare-prices\/?"
-                    title='Invalid link. Please check the description above for an example.'
-                    required
-                />
-                <button type='submit'>Add Game</button>
             </form>
         </>
     )
