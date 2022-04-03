27da75e657faf1524ab7191a0b141058af3ffdfa
Christopher Fargere <christopher.fargere@gmail.com>
2022-04-03 19:27:25 +0200

    mon dernier espoir pour un monde meilleur

--- pages/api/games/update.ts
+++ pages/api/games/update.ts
@@ -20,7 +20,7 @@ export default async function handler(
     if (gamesToUpdate.length > 0) {
         for (const game of gamesToUpdate) {
             await Promise.all([
-                fetch(game.url)
+                await fetch(game.url)
                     .then(res => res.text())
                     .then(async contents => {
                         const newPrice = getPrice(contents)
