58de16614b1a3230beb66025e9a54f413ee723c6
Christopher Fargere <christopher@omeheart.fr>
2022-07-10 12:03:54 +0200

    sort filter + cleaned code

--- pages/api/games/store.ts
+++ pages/api/games/store.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import prisma from "../../../utils/prisma"
+import prisma from "../../../lib/prisma"
 import { Platform } from "@prisma/client"
 import * as cheerio from "cheerio"
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,20 @@
 import type { NextPage } from "next";
 import useSWR, { mutate } from 'swr'
-import fetcher from "../utils/fetcher";
+import fetcher from "../lib/fetcher";
 
-import { Game } from "../utils/game";
+import { Game } from "@prisma/client";
 import { GameCard } from "../components/game-card";
 import { AddGameCard } from "../components/add-game-card"
 import styles from "../styles/Home.module.css"
+import { ChangeEvent, useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-    const { data: games, error } = useSWR('/api/games/get', fetcher, {
+    const { data, error } = useSWR('/api/games/get', fetcher, {
         revalidateOnFocus: false,
         revalidateOnReconnect: false
     })
+    const [games, setGames] = useState([])
+    useEffect(() => { setGames(data) })
 
     // TODO: I should probably just check the game updated when doing the GET query
     useSWR(() => {
@@ -24,17 +27,62 @@ const Home: NextPage = () => {
     }), {
         onSuccess: async (data) => {
             const updatedGames = await data.json()
-            mutate('/api/games/get', async (games: Game[]) => [...updatedGames])
+            mutate('/api/games/get', async () => [...updatedGames])
         },
         revalidateOnFocus: false,
         revalidateOnReconnect: false
     })
+
+    const sortGames = (event: ChangeEvent<HTMLSelectElement>) => {
+        console.log(event.target.value)
+        switch (event.target.value) {
+            case 'game_asc':
+                games.sort((a: Game, b: Game) => {
+                    if (a.name < b.name) return -1
+                    if (a.name > b.name) return 1
+                    return 0
+                })
+                break;
+            case 'game_desc':
+                games.sort((a: Game, b: Game) => {
+                    if (a.name < b.name) return 1
+                    if (a.name > b.name) return -1
+                    return 0
+                })
+                break;
+            case 'price_asc':
+                games.sort((a: Game, b: Game) => {
+                    if (a.bestPrice < b.bestPrice) return -1
+                    if (a.bestPrice > b.bestPrice) return 1
+                    return 0
+                })
+                break;
+            case 'price_desc':
+                games.sort((a: Game, b: Game) => {
+                    if (a.bestPrice < b.bestPrice) return 1
+                    if (a.bestPrice > b.bestPrice) return -1
+                    return 0
+                })
+                break;
+        }
+        setGames([...games])
+    }
 
     if (error) return <div>failed to load</div>
     if (!games) return <div>loading...</div>
     return (
         <>
             <div className={styles.container}>
+                <div className="flex justify-center">
+                    Sort
+                    <select onChange={sortGames}>
+                        <option value='game_asc'>Game (a-z)</option>
+                        <option value='game_desc'>Game (z-a)</option>
+                        <option value='price_asc'>Price (smallest)</option>
+                        <option value='price_desc'>Price (biggest)</option>
+                    </select>
+                </div>
+
                 <div className={"flex justify-evenly " + (games.length > 0? "flex-wrap": "flex-col")}>
                     {
                         games.length > 0
