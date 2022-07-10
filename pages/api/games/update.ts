d43864fd472236934502eabaa6f931993af7526c
Christopher Fargere <christopher.fargere@gmail.com>
2022-07-10 15:34:59 +0200

    tring fix of prisma relative path for heroku

--- pages/api/games/update.ts
+++ pages/api/games/update.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import prisma from "../../../lib/prisma"
+import prisma from "lib/prisma"
 import { Game } from "@prisma/client";
 import * as cheerio from "cheerio"
 import moment from "moment";
