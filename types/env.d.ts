7cd74b1b52e0f10e2bd27a48a0b02a6251f8cf1d
Christopher Fargere <christopher.fargere@gmail.com>
2022-09-28 14:17:17 +0200

    added types for env variables

---
+++ types/env.d.ts
@@ -0,0 +1,8 @@
+namespace NodeJS {
+    interface ProcessEnv {
+        NEXT_PUBLIC_LOCALE: string
+        NEXT_PUBLIC_DAYS_BEFORE_STALE: number
+        NEXT_PUBLIC_MINUTES_BEFORE_STALE: number
+        NEXT_PUBLIC_TIMEOUT_BETWEEN_QUERIES: number
+    }
+}
