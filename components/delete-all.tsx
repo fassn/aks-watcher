26b3006ba62a26339068002db312484e04a72367
Christopher Fargere <christopher.fargere@gmail.com>
2023-01-12 00:41:49 +0100

    Game-add form as a modal instead of a page

--- components/delete-all.tsx
+++ components/delete-all.tsx
@@ -13,7 +13,7 @@ export const DeleteAll = ({ setFlash }: DeleteAllProps) => {
     const { games, mutate } = useGames()
 
     const [isDeleting, setIsDeleting] = useState(false)
-    const [modalOpen, setModalOpen] = useState(false)
+    const [modalShow, setModalShow] = useState(false)
 
     const onClick = async () => {
         if (isDeleting) {
@@ -25,7 +25,7 @@ export const DeleteAll = ({ setFlash }: DeleteAllProps) => {
         }
 
         setIsDeleting(true)
-        setModalOpen(false)
+        setModalShow(false)
         setFlash({ message: 'Deletion has started. This may take a while. Please do not reload the page.', severity: 'info', delay: 5000 })
 
         const res = await fetch('/api/games/delete', {
@@ -45,23 +45,23 @@ export const DeleteAll = ({ setFlash }: DeleteAllProps) => {
         setTimeout(() => setFlash({}), 5000)
     }
 
-    const openModal = () => {
-        setModalOpen(!modalOpen)
+    const showModal = () => {
+        setModalShow(!modalShow)
     }
 
     const closeModal = () => {
-        setModalOpen(false)
+        setModalShow(false)
     }
 
     return (
         <div className="w-full flex justify-end">
-        <button className="flex items-center mr" onClick={openModal} id="delete_all">
+        <button className="flex items-center mr" onClick={showModal} id="delete_all">
             <span className="font-josephin mr-2">
                 Delete all
             </span>
             <svg xmlns="http://www.w3.org/2000/svg" width="18" viewBox="0 0 32 32"><g data-name="70-Trash"><path d="m29.89 6.55-1-2A1 1 0 0 0 28 4h-7V2a2 2 0 0 0-2-2h-6a2 2 0 0 0-2 2v2H4a1 1 0 0 0-.89.55l-1 2A1 1 0 0 0 3 8h2v22a2 2 0 0 0 .47 1.41A2 2 0 0 0 7 32h18a2 2 0 0 0 2-2V8h2a1 1 0 0 0 .89-1.45zM13 2h6v2h-6zm12 28H7V8h18z" /><path d="M17 26V10a2 2 0 0 0-2 2l.06 14H15v2a2 2 0 0 0 2-2zM22 26V10a2 2 0 0 0-2 2l.06 14H20v2a2 2 0 0 0 2-2zM12 26V10a2 2 0 0 0-2 2l.06 14H10v2a2 2 0 0 0 2-2z" /></g></svg>
         </button>
-        <Modal open={modalOpen} onRequestClose={closeModal} className="w-80 h-48">
+        <Modal show={modalShow} onRequestClose={closeModal} className="w-80 h-48">
             <p className="h-1/2">Are you sure you want to remove all your games?</p>
             <div className="flex h-1/2">
                 <button onClick={onClick} className="flex-none w-full justify-center self-end bg-deep-blue text-cream font-semibold py-2 px-4 border border-blue-500 hover:border-transparent rounded">Confirm</button>
