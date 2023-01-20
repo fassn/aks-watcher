3f0cacd88d5b09b624805b86aa901fb13e0ae111
Christopher Fargere <christopher.fargere@gmail.com>
2023-01-20 11:09:08 +0100

    splitted header's game add button into a component

---
+++ components/add-game-button.tsx
@@ -0,0 +1,42 @@
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import ReactModal from "react-modal";
+import { GameForm } from "./game-form";
+
+export const AddGameButton = () => {
+    const {data: session} = useSession()
+    const [modalShow, setModalShow] = useState(false)
+
+    useEffect(() => {
+        ReactModal.setAppElement(document.getElementById('root')!); //non-null assertion
+    }, [])
+
+    const openModal = () => {
+        setModalShow(!modalShow)
+    }
+
+    const closeModal = () => {
+        setModalShow(false)
+    }
+
+    if (session) {
+        return (
+            <div className="flex w-full justify-end">
+                <button id='add_game_header' onClick={openModal} className="flex justify-center items-center w-11 h-11">
+                    <svg className="h-6 w-6 text-cream"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4"/>
+                    </svg>
+                </button>
+                    <ReactModal
+                        className='absolute top-1/3 overflow-auto left-[40%] w-1/5 rounded p-5'
+                        overlayClassName='fixed inset-0 backdrop-blur-[5px]'
+                        isOpen={modalShow}
+                        onRequestClose={closeModal}
+                    >
+                        <GameForm closeModal={closeModal} />
+                    </ReactModal>
+            </div>
+        )
+    }
+    return <></>
+}
--- components/header.tsx
+++ components/header.tsx
@@ -1,48 +1,8 @@
-import { useSession } from "next-auth/react"
 import Link from "next/link"
-import { useEffect, useState } from "react"
-import { GameForm } from "./game-form"
 import SignIn from "./signin"
-import ReactModal from "react-modal"
+import { AddGameButton } from "./add-game-button"
 
 export const Header = () => {
-    const {data: session} = useSession()
-    const [modalShow, setModalShow] = useState(false)
-
-    const openModal = () => {
-        setModalShow(!modalShow)
-    }
-
-    const closeModal = () => {
-        setModalShow(false)
-    }
-
-    const AddGameButton = () => {
-        useEffect(() => {
-            ReactModal.setAppElement(document.getElementById('root')!); //non-null assertion
-        }, [])
-
-        if (session) {
-            return (
-                <div className="flex w-full justify-end">
-                    <button id='add_game_header' onClick={openModal} className="flex justify-center items-center w-11 h-11">
-                        <svg className="h-6 w-6 text-cream"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4"/>
-                        </svg>
-                    </button>
-                        <ReactModal
-                            className='absolute top-1/3 overflow-auto left-[40%] w-1/5 rounded p-5'
-                            overlayClassName='fixed inset-0 backdrop-blur-[5px]'
-                            isOpen={modalShow}
-                            onRequestClose={closeModal}
-                        >
-                            <GameForm closeModal={closeModal} />
-                        </ReactModal>
-                </div>
-            )
-        }
-        return <></>
-    }
 
     return (
         <header className="flex w-full fixed top-0 z-50 h-12 items-center bg-deep-blue pl-8 pr-4">
