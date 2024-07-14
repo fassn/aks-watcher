1daf3979a0ac4ffe08484d1a46a1e379a0983829
Christopher Fargere <christopher.fargere@gmail.com>
2024-07-14 22:04:50 +0200

    removed firebase files

--- pages/api/queues/update.ts
+++ pages/api/queues/update.ts
@@ -1,11 +1,11 @@
 import { Game } from "@prisma/client";
 import { Queue } from "quirrel/next";
-import { timeout, updateGame } from "../shared";
+import { timeout, updatePrice } from "../shared";
 
-export default Queue('api/queues/update', async (games: { id: string, url: string }[]) => {
+export default Queue('api/queues/update', async (games: Game[]) => {
     for (const game of games) {
         await Promise.all([
-            await updateGame(game.id, game.url),
+            await updatePrice(game),
             timeout(process.env.NEXT_PUBLIC_TIMEOUT_BETWEEN_QUERIES)
         ])
     }
